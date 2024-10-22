e50bf73c0653ebc29c549e6f628a31d37a4ee179
leilei405 <1601178425@qq.com>
2024-10-22 20:07:43 +0800

    feat: ✨判断该字典是否存在该属性  返回boolean类型

--- Map/index.js
+++ Map/index.js
@@ -1,9 +1,18 @@
+/*
+ * @Author: leilei405 1601178425@qq.com
+ * @Date: 2024-10-21 19:16:28
+ * @LastEditors: leilei405 1601178425@qq.com
+ * @LastEditTime: 2024-10-22 20:06:08
+ * @FilePath: \algorithm\Map\index.js
+ */
 const m = new Map();
 
 // 增
 m.set("name", "leilei");
 m.set("age", 18);
 m.set("sex", "male");
+
+console.log(m.has("dog"));
 
 // 删
 m.delete("name");
