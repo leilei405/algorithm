0078e8c077c21a8f82c177d9e0ff8312d8d289bb
leilei405 <1601178425@qq.com>
2024-10-23 21:36:34 +0800

    feat: ✨二叉树的先中后序变量练习  递版

---
+++ tree/data.js
@@ -0,0 +1,34 @@
+// 一个二叉树数据解构
+const data = {
+  val: 1,
+  left: {
+    val: 2,
+    left: {
+      val: 4,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 5,
+      left: null,
+      right: null,
+    },
+  },
+  right: {
+    val: 3,
+    left: {
+      val: 6,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 7,
+      left: null,
+      right: null,
+    },
+  },
+};
+
+module.exports = {
+  data,
+};
