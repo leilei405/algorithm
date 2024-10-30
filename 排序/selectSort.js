195fdafa5fae4081e0e90d62635982e1b00f0871
leilei405 <1601178425@qq.com>
2024-10-30 22:53:01 +0800

    feat: ✨选择排序

--- 排序/selectSort.js
+++ 排序/selectSort.js
@@ -1,1 +1,25 @@
 // 选择排序
+Array.prototype.selectSort = function () {
+  let sortedArray = this.slice();
+  let len = sortedArray.length;
+
+  for (let i = 0; i < len - 1; i++) {
+    let indexMin = i;
+    for (let j = i; j < len; j++) {
+      if (sortedArray[j] < sortedArray[indexMin]) {
+        indexMin = j;
+      }
+    }
+
+    if (indexMin !== i) {
+      const temp = sortedArray[i];
+      sortedArray[i] = sortedArray[indexMin];
+      sortedArray[indexMin] = temp;
+    }
+  }
+
+  return sortedArray;
+};
+
+const arr = [21, 4, 68, 32, 12];
+console.log(arr.selectSort());
